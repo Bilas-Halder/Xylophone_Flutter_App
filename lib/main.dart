2e14e789d0fde8f749368c7ac93bc930d5aaac5a
Bilas-Halder <bilas8642@gmail.com>
2022-01-22 15:15:02 +0600

    7 playable audio added

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,114 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int audioNum){
+    final player = AudioCache();
+    player.play('note$audioNum.wav');
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: TextButton(
-              onPressed: (){
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click Me'),
+            child: Column(
+              children: [
+                Expanded(
+                  child: TextButton(
+                    onPressed: (){
+                      playSound(1);
+                    },
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                    ),
+                    child: Container(
+                      color: Colors.teal[900],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    onPressed: (){
+                      playSound(2);
+                    },
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                    ),
+                    child: Container(
+                      color: Colors.teal[700],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    onPressed: (){
+                      playSound(3);
+                    },
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                    ),
+                    child: Container(
+                      color: Colors.teal[500],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: (){
+                      playSound(4);
+                    },
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                    ),
+                    child: Container(
+                      color: Colors.teal[400],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: (){
+                      playSound(5);
+                    },
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                    ),
+                    child: Container(
+                      color: Colors.teal[300],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: (){
+                      playSound(6);
+                    },
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                    ),
+                    child: Container(
+                      color: Colors.teal[200],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: (){
+                      playSound(7);
+                    },
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                    ),
+                    child: Container(
+                      color: Colors.teal[100],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
